ff68a239cbebcbeeb9de12c84058e79fd502a9b7
Valen-MsCool <82281861+Valen-MsCool@users.noreply.github.com>
2022-06-14 13:48:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 //https://teachablemachine.withgoogle.com/models/c_u_5tM2h/
+
+
+prediction1 = "";
+
+
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+Webcam.snap(function(data_uri){
+   document.getElementById('result').innerHTML='<img id="captured_image" src="'+data_uri+'">';
+});
+}
+console.log("ml5 version: ", ml5.version);
+
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/c_u_5tM2h/model.json',modelLoaded);
+
+function modelLoaded(){
+console.log("Model Loaded");
+}
+
+function check(){
+img = document.getElementById('captured_image');
+classifier.classify(img,gotResult);
+   }
+
+function gotResult(error,results){
+if (error){
+console.error(error);
+}else{
+console.log(results);
+document.getElementById('result_emotion_name').innerHTML=results[0].label;
+document.getElementById('result_emotion_name2').innerHTML=results[1].label;
+prediction1=results[0].label;
+
+speak();
+if (results[0].label == "Peace"){
+document.getElementById('update_emoji').innerHTML = "✌";
+}
+if (results[0].label == "Perfect"){
+   document.getElementById('update_emoji').innerHTML = "👌";
+}
+if (results[0].label == "Thumbs Up"){
+   document.getElementById('update_emoji').innerHTML = "👍";
+   }
+}
+
+}
